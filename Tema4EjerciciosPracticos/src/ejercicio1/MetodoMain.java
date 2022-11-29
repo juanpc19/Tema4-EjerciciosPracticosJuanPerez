85300443f45730eaf54670bb770c38c4b60bfbc2
jperez <jperez@106-20.info2>
2022-11-29 12:51:46 +0100

    ej1 mitad

--- Tema4EjerciciosPracticos/src/ejercicio1/MetodoMain.java
+++ Tema4EjerciciosPracticos/src/ejercicio1/MetodoMain.java
@@ -1,10 +1,33 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class MetodoMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Declaro variable para llamar a funcion
+		int numeroIntroducido;
 
+		//Creo escaner
+		Scanner dogma = new Scanner(System.in);
+		
+		//Creo objeto (funciones) con el que llamar a las funciones que esten dentro de la clase (Numeros) que contiene las funciones 
+		Numeros funciones = new Numeros();
+		
+		//Solicito valor para variable
+		System.out.println("Introduzca un numero entero positivo mayor a 1: ");
+		
+		//Doy valor a variable con escaner
+		numeroIntroducido = dogma.nextInt();
+		
+		//Imprimo mensaje con el objeto "funciones" que llamara a las funciones que esten dentro de la clase "Numeros"
+		//En este caso le indico que solo llame a una funcion de las dos que hay en la clase, a "esPrimo"
+		//Añado un ternario para mejor presentacion
+		System.out.println(funciones.esPrimo(numeroIntroducido) ? "El numero es primo" : "El numero no es primo");
+
+		
+		dogma.close();
 	}
 
 }
--- Tema4EjerciciosPracticos/src/ejercicio1/Numeros.java
+++ Tema4EjerciciosPracticos/src/ejercicio1/Numeros.java
@@ -1,7 +1,22 @@
 package ejercicio1;
 
 public class Numeros {
+	//aqui se declararia una variable o una constante global
 	
-	
-	
+	boolean esPrimo (int numero) {
+		boolean primo = true; 
+		
+		if (numero==1) {
+			primo=false;
+		} else {
+			for (int i=2; i<numero; i++) {
+		
+			if (numero%i==0) {
+				primo=false;
+				break;
+				}
+			}
+		}
+		return (primo);
+	}
 }
