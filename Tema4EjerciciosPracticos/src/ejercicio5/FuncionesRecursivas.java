b49d2d9b91ad45575fa1616af219b1dfe4d167c1
jperez <jperez@106-20.info2>
2022-12-01 14:40:19 +0100

    dada

--- Tema4EjerciciosPracticos/src/ejercicio5/FuncionesRecursivas.java
+++ Tema4EjerciciosPracticos/src/ejercicio5/FuncionesRecursivas.java
@@ -1,5 +1,43 @@
 package ejercicio5;
 
 public class FuncionesRecursivas {
+	
+	//Valor de entrada dado en MetodoMain que ira modificandose con la funcion recursiva
+	static int sumatorio (int numero) { 
+		
+		//Declaro variable y la inicializo con valor igual a parametro entrada numero
+		int resultado=numero;
+		
+		//Establezco condicion si numero igual a 0 
+		if (numero == 0) {
+			//Que de cumplirse dara a resultado valor igual a 0
+			resultado=0;
+		}
+		else {
+			//De no cumplirse la condicion anterior, resultado tomara el valor de
+			//numero y llamara a la funcion sumatorio, que se ejecutara restando 1
+			//al valor de numero, este nuevo valor se sumara a resultado otra vez y 
+			//acto seguido se volvera a llamar a la funcion sumatorio que se ejecutara 
+			//restando 1 al valor de numero, seguira haciendo esto hasta que numero 
+			//tome de valor 0 lo cual cumplira la condicion de if al inicio ed la funcion
+			//finalizando la funcion dandonos un return 
+			//con la variable resultado conteniendo el valor deseado
+				resultado=numero+sumatorio(numero-1);
+			}
+			return (resultado);
+		}
+	
+	static double potencia (double a, int n) {
+		
+		double resultado=0;
+		
+		if (n==0) {
+			resultado=1;
+		} else {
+		resultado=a+potencia(n-1);	//probar cast
+			
+		}
+		return (resultado);
+	}
 
 }
--- Tema4EjerciciosPracticos/src/ejercicio5/MetodoMain.java
+++ Tema4EjerciciosPracticos/src/ejercicio5/MetodoMain.java
@@ -1,10 +1,35 @@
 package ejercicio5;
 
+import java.util.Scanner;
+
 public class MetodoMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int numeroIntroducido;
+		
+		double a;
+		
+		int n;
+		
+		Scanner dogma = new Scanner(System.in);
+		
+		System.out.println("Introduzca un numero entero :");
+		
+		numeroIntroducido=dogma.nextInt();
 
+		System.out.println("La suma de todos los anteriores al numero "
+				+ "introducido mas el introducido es: " + FuncionesRecursivas.sumatorio(numeroIntroducido));
+		
+		System.out.println("Introduzca un numero que sera la base :");
+		
+		a = dogma.nextDouble();
+		
+		System.out.println("Introduzca un numero que sera la exponente :");
+		
+		n =  dogma.nextInt();
+		
+		dogma.close();
 	}
 
 }
